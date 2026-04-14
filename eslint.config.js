efb48c097bf9d4b827746928113c33d2ea9d372d
Rick <flintweather@gmail.com>
2026-04-13 22:24:12 -0500

    Phase 0: chore: Adjust linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,6 @@ import prettierConfig from "eslint-config-prettier";
 import sonarjs from "eslint-plugin-sonarjs";
 import jsdoc from "eslint-plugin-jsdoc";
 
-
-
 export default defineConfig([
   js.configs.recommended,
   // sonarjs.configs.recommended, -- for use if not use plugins.sonarjs and specific rules
@@ -27,21 +25,21 @@ export default defineConfig([
     plugins: {
       jsdoc,
       // prettier: prettier,
-      sonarjs, 
+      sonarjs,
     },
 
-    "rules": {
+    rules: {
       "no-unused-vars": "warn",
       "no-console": "warn",
       "prettier/prettier": [
         "error",
         {
-          "endOfLine": "auto"
-        }
+          endOfLine: "auto",
+        },
       ],
-      "indent": ["error", 2],
-      "quotes": ["error", "double"],
-      "semi": ["error", "always"],
+      indent: ["error", 2],
+      quotes: ["error", "double", { allowTemplateLiterals: true }],
+      semi: ["error", "always"],
       "no-trailing-spaces": "error",
       "eol-last": ["error", "always"],
       "comma-dangle": ["error", "always-multiline"],
@@ -49,11 +47,16 @@ export default defineConfig([
       "object-curly-spacing": ["error", "always"],
       "array-bracket-spacing": ["error", "never"],
       "block-spacing": ["error", "always"],
-      "keyword-spacing": ["error", { "before": true, "after": true }],
-      "space-before-function-paren": ["error", "never"],
+      "keyword-spacing": ["error", { before: true, after: true }],
+      "space-before-function-paren": ["error", {
+        "anonymous": "ignore",
+        "named": "always",
+        "asyncArrow": "always"
+      }],
+      "space-infix-ops": "error",
 
       // Complexity rules
-      "complexity": ["warn", { "max": 15 }],
+      complexity: ["warn", { max: 15 }],
       "max-statements": ["warn", 20],
       "sonarjs/cognitive-complexity": ["warn", 15],
 
